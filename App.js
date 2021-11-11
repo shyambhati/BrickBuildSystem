86ae6ddaf29e36f35f4c4aa063ff28ee89fc847b
shyambhati <shyambhati97@gmail.com>
2021-11-11 12:00:11 +0530

    Commit all files

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        >"Heloo"
+      </Text> 
+    </View>
+  );
+};
+
+
+export default App;
